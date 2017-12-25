3a9a7bebdaaac20f64944dbb959445017c31b23a
Marc Hermans <OrionDevelopment@outlook.com>
2017-12-25 23:42:37 +0100

    Fixing the forge version dependency.

--- src/main/java/com/minecolonies/blockout/util/Constants.java
+++ src/main/java/com/minecolonies/blockout/util/Constants.java
@@ -7,5 +7,5 @@ public class Constants
 
     public static final String VERSION       = "@VERSION@";
     public static final String MC_VERSION    = "@MCVERSION@";
-    public static final String FORGE_VERSION = "@FORGEVERSION@";
+    public static final String FORGE_VERSION = "required:forge@[@FORGEVERSION@,);";
 }
