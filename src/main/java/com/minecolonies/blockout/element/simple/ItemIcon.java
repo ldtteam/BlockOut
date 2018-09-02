9d5193920c26f41a939930b2b492aefbb4900ea0
Marc Hermans <OrionDevelopment@outlook.com>
2018-09-02 09:56:24 +0200

    Making the ItemIcon change track changes to his properties.

--- src/main/java/com/minecolonies/blockout/element/simple/ItemIcon.java
+++ src/main/java/com/minecolonies/blockout/element/simple/ItemIcon.java
@@ -9,6 +9,7 @@ import com.minecolonies.blockout.core.element.values.Alignment;
 import com.minecolonies.blockout.core.element.values.AxisDistance;
 import com.minecolonies.blockout.core.element.values.Dock;
 import com.minecolonies.blockout.core.factory.IUIElementFactory;
+import com.minecolonies.blockout.core.management.update.IUpdateManager;
 import com.minecolonies.blockout.element.core.AbstractSimpleUIElement;
 import com.minecolonies.blockout.loader.IUIElementData;
 import com.minecolonies.blockout.loader.IUIElementDataBuilder;
@@ -58,6 +59,17 @@ public class ItemIcon extends AbstractSimpleUIElement implements IDrawableUIElem
     {
         super(KEY_ITEM, style, id, parent, alignments, dock, margin, elementSize, dataContext, visible, enabled);
         this.iconResource = iconResource;
+    }
+
+    @Override
+    public void update(@NotNull final IUpdateManager updateManager)
+    {
+        super.update(updateManager);
+
+        if (iconResource.hasChanged(getDataContext()))
+        {
+            updateManager.markDirty();
+        }
     }
 
     @SideOnly(Side.CLIENT)
