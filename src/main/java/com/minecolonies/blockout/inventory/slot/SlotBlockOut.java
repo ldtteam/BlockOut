e430c4623cdf4d20b7dd6968371172abb0bad7e9
Marc Hermans <OrionDevelopment@outlook.com>
2018-09-03 12:05:38 +0200

    Shift click support should now be supported completedly.

---
+++ src/main/java/com/minecolonies/blockout/inventory/slot/SlotBlockOut.java
@@ -0,0 +1,33 @@
+package com.minecolonies.blockout.inventory.slot;
+
+import com.minecolonies.blockout.element.simple.Slot;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.SlotItemHandler;
+import org.jetbrains.annotations.NotNull;
+
+public class SlotBlockOut extends SlotItemHandler
+{
+    private final Slot uiSlotInstance;
+
+    public SlotBlockOut(@NotNull final IItemHandler itemHandler, @NotNull final Slot uiSlotInstance)
+    {
+        super(itemHandler,
+          uiSlotInstance.getInventoryIndex(),
+          (int) uiSlotInstance.getAbsoluteBoundingBox().getLocalOrigin().getX() + 1,
+          (int) uiSlotInstance.getAbsoluteBoundingBox().getLocalOrigin().getY() + 1);
+
+        this.uiSlotInstance = uiSlotInstance;
+    }
+
+    @Override
+    public boolean canTakeStack(final EntityPlayer playerIn)
+    {
+        return getUiSlotInstance().isEnabled() && super.canTakeStack(playerIn);
+    }
+
+    public Slot getUiSlotInstance()
+    {
+        return uiSlotInstance;
+    }
+}
