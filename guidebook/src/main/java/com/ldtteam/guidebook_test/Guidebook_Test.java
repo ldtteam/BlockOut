5f57bf644c55fd4457a606909eb8d604ad60d1d4
Marc Hermans <oriondevelopment@outlook.com>
2019-01-27 08:57:32 +0100

    Updating project structure.

--- guidebook/src/main/java/com/ldtteam/guidebook_test/Guidebook_Test.java
+++ guidebook/src/main/java/com/ldtteam/guidebook_test/Guidebook_Test.java
@@ -13,7 +13,7 @@ public class Guidebook_Test
     public static final String NAME    = "Guidebook Test";
     public static final String VERSION = "1.0";
 
-    private static Logger logger;
+    public static Logger logger;
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
